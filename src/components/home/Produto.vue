d684f0430ed4400801ac2ea0084e2d9a7cca4281
AlexHPasquini <alex_hpasquini@hotmail.com>
2024-09-19 21:49:24 -0300

    Criado o Produto como uma Prop e adicionado tambem o modo de avaliação de cada

---
+++ src/components/home/Produto.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="produto">
+      <img :src="produto.imagem" :alt="produto.nome" />
+      <div>{{ produto.nome }}</div>
+      <div>{{ produto.preco }}</div>
+      <div>
+        <span v-for="n in 5" :key="n">{{ n <= produto.avaliacao ? '★' : '☆' }}</span>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ["produto"], 
+  };
+  </script>
+  
+  <style scoped>
+  .produto {
+    text-align: center;
+    margin: 10px;
+  }
+  </style>
+  
