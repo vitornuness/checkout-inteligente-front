c0be0998993ada63e0d77df09853bab4dedc20ce
vinirainho <rainhovinicius@gmail.com>
2024-09-24 21:11:21 -0300

    adicionei o footer e adicionei tambem a navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,193 +4,82 @@ import { session } from "../session";
 </script>
 
 <template>
-    <div class="container-fluid mb-2 p-2 header">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container d-flex justify-content-between">
-                <div class="col-md-2">
-                    <RouterLink to="/" class="navbar-brand text-white"
-                        >Loja</RouterLink
-                    >
-                </div>
-                <div class="col-md-6">
-                    <form class="d-flex" role="search">
-                        <input
-                            class="form-control"
-                            type="search"
-                            placeholder="Procure em toda loja"
-                            aria-label="Procure em toda loja"
-                        />
-                        <button class="btn" type="submit">
-                            <i class="bi bi-search text-white"></i>
-                        </button>
-                    </form>
-                </div>
-                <div>
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <i class="bi bi-list"></i>
-                    </button>
-                    <div
-                        class="collapse navbar-collapse"
-                        id="navbarSupportedContent"
-                    >
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-white">
-                            <div v-if="!session().token">
-                                <li class="nav-item d-flex mt-2">
-                                    <RouterLink
-                                        to="/login"
-                                        class="nav-link text-white"
-                                        >Entrar</RouterLink
-                                    >
-                                    <span
-                                        class="row align-items-start mt-2 mx-2"
-                                        >|</span
-                                    >
-                                    <RouterLink
-                                        to="/signup"
-                                        class="nav-link text-white"
-                                        >Registrar-se</RouterLink
-                                    >
-                                </li>
-                            </div>
-                            <div v-else class="d-flex">
-                                <li class="nav-item">
-                                    <RouterLink
-                                        to="cart"
-                                        class="nav-link text-white"
-                                    >
-                                        <i class="bi bi-cart3"></i>
-                                    </RouterLink>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a
-                                        class="nav-link text-white dropdown-toggle"
-                                        href="#"
-                                        role="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false"
-                                    >
-                                        <i class="bi bi-person-fill"></i>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a class="dropdown-item" href="#"
-                                                >Perfil</a
-                                            >
-                                        </li>
-                                        <li><hr class="dropdown-divider" /></li>
-                                        <div v-if="session().user.admin">
-                                            <li>
-                                                <router-link
-                                                    class="dropdown-item"
-                                                    to="/products"
-                                                    >Produtos</router-link
-                                                >
-                                            </li>
-                                            <li>
-                                                <router-link
-                                                    class="dropdown-item"
-                                                    to="/categories"
-                                                    >Categorias</router-link
-                                                >
-                                            </li>
-                                            <li>
-                                                <router-link
-                                                    class="dropdown-item"
-                                                    to="/campaigns"
-                                                    >Campanhas</router-link
-                                                >
-                                            </li>
-                                            <li>
-                                                <router-link
-                                                    class="dropdown-item"
-                                                    to="/orders"
-                                                    >Pedidos</router-link
-                                                >
-                                            </li>
-                                            <li>
-                                                <router-link
-                                                    class="dropdown-item"
-                                                    to="/users"
-                                                    >Usuários</router-link
-                                                >
-                                            </li>
-                                        </div>
-                                        <li>
-                                            <button
-                                                class="dropdown-item"
-                                                @click="logout()"
-                                            >
-                                                Sair
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </div>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
+  <header>
+    <div
+      class="topo container d-flex justify-content-between align-items-center"
+    >
+      <h1>SHOP</h1>
+      <div class="pesquisa">
+        <input type="text" placeholder="Pesquisar um produto" />
+        <button class="btn btn-primary">Pesquisar</button>
+      </div>
+      <div class="login">
+        <a href="#" class="btn btn-outline-secondary">Entrar ou Registrar</a>
+      </div>
     </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "home",
-    methods: {
-        logout() {
-            session().clearData();
-            location.reload();
-        },
+  name: "home",
+  methods: {
+    logout() {
+      session().clearData();
+      location.reload();
     },
+  },
 };
 </script>
 
 <style>
-.text-white {
-    color: #fff;
-}
-.header {
-    background: #13293d;
-    box-shadow: 0 1px 4px #000;
-}
-
-.navbar-toggler {
-    color: #fff;
-    border: none;
+header {
+  background-color: #2b2b2b;
+  color: white;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.bi-search {
-    font-size: 14pt !important;
+header h1 {
+  margin: 0;
 }
 
-i,
-.bi {
-    font-size: 20pt;
+.pesquisa {
+  display: flex; /* Faz com que o input e o button fiquem lado a lado */
+  align-items: center; /* Alinha verticalmente o input e o botão */
 }
 
-a:hover,
-i:hover,
-.bi:hover {
-    color: #9193b4 !important;
-    transition: 0.5s ease-in-out;
+.pesquisa input {
+  padding: 5px;
+  width: 300px;
+  border: none;
+  border-radius: 4px 0 0 4px; /* Borda arredondada para o input */
 }
 
-.dropdown-item:active {
-    background: #9193b4;
+.pesquisa button {
+  padding: 5px 10px;
+  background-color: #ff9900;
+  border: none;
+  color: white;
+  cursor: pointer;
+  border-radius: 0 4px 4px 0; /* Borda arredondada no lado direito do botão */
 }
 
-.right-nav-side {
-    display: flex;
-    justify-content: end;
-    align-items: end;
+.pesquisa button:hover {
+  background-color: #e68a00;
+}
+
+.login a {
+  padding: 5px 10px;
+  border: 1px solid white;
+  color: white;
+  text-decoration: none;
+  border-radius: 4px;
+}
+
+.login a:hover {
+  background-color: #444;
 }
 </style>
