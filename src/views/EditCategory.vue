ba2db3330b23d9e62c8bb0f0cc214c3a66c5a2e3
VitorNuness <vitor1nuness@gmail.com>
2024-09-26 08:35:03 -0300

    adicionando o envio da imagem para criar e atualizar a categoria

--- src/views/EditCategory.vue
+++ src/views/EditCategory.vue
@@ -2,7 +2,15 @@
     <div class="container center">
         <h1>Editar categoria</h1>
 
-        <div class="form row g-3 m-4 needs-validation" novalidate>
+        <div v-if="submitted" class="alert alert-success">
+            Categoria atualizada com sucesso.
+        </div>
+
+        <div
+            v-if="category"
+            class="form row g-3 m-4 needs-validation"
+            novalidate
+        >
             <div class="row">
                 <label for="name" class="form-label">Nome</label>
                 <input
@@ -13,6 +21,25 @@
                     placeholder="Categoria exemplo"
                     required
                     v-model="category.name"
+                />
+            </div>
+            <div class="row">
+                <label for="image" class="form-label">Imagem</label>
+                <div class="row my-4">
+                    <img
+                        :src="fileUrl ?? category.imageUrl"
+                        :alt="category.name"
+                        style="width: auto; height: 20vh"
+                    />
+                </div>
+                <input
+                    type="file"
+                    class="form-control mb-4"
+                    id="image"
+                    ref="image"
+                    accept="image/*"
+                    required
+                    @change="setImage"
                 />
             </div>
             <div class="row my-4 g-4">
@@ -31,16 +58,17 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import CategoryDataService from "../services/CategoryDataService";
 
 export default {
     name: "category-edit",
     data() {
         return {
-            category: {
-                id: "",
-                name: "",
-            },
+            submitted: false,
+            category: ref(null),
+            file: null,
+            fileUrl: null,
         };
     },
     methods: {
@@ -54,18 +82,36 @@ export default {
                 });
         },
         updateCategory() {
+            var formData = new FormData();
+            formData.append("file", this.file);
+
             var data = {
                 id: this.category.id,
                 name: this.category.name,
+                image: formData.get("file"),
             };
 
             CategoryDataService.update(data.id, data)
                 .then((res) => {
+                    this.submitted = true;
+                    this.file = null;
+                    this.fileUrl = null;
+                    this.$refs.image.value = null;
                     this.getCategory(data.id);
                 })
                 .catch((err) => {
                     console.log(err);
                 });
+        },
+        setImage() {
+            var file = this.$refs.image.files.item(0);
+            this.file = file;
+
+            if (file) {
+                this.fileUrl = URL.createObjectURL(file);
+            } else {
+                this.fileUrl = null;
+            }
         },
         deleteCategory() {
             CategoryDataService.delete(this.category.id)
