e74b6d659e14ab11ef4d384c5919d5ccb5656eb6
VitorNuness <vitor1nuness@gmail.com>
2024-09-28 16:24:47 -0300

    corrigindo footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,14 @@
 <template>
-  <footer class="text-center mt-5">
-    <p>© 2024 Loja Online. Todos os direitos reservados.</p>
-  </footer>
+    <footer class="text-center mt-5">
+        <p>© 2024 Loja Online. Todos os direitos reservados.</p>
+    </footer>
 </template>
 
 <style>
 footer {
-  background-color: #2b2b2b;
-  color: white;
-  text-align: center;
-  padding: 15px;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
+    background-color: #2b2b2b;
+    color: white;
+    text-align: center;
+    padding: 15px;
 }
 </style>
