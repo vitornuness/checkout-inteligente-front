d1c26f628431bd606372f57135b58631de32a1f1
VitorNuness <vitor1nuness@gmail.com>
2024-09-28 10:26:16 -0300

    ajustando lista de produtos ao editar ou criar uma campanha, pois o nome dos produtos estava ultrapassando o tamanho desejado

--- src/components/campaign/ProductSelector.vue
+++ src/components/campaign/ProductSelector.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="row my-4 g-4">
-        <label for="products">Selecione os produtos para a campanha</label>
-        <div class="row row-cols-3 mt-4">
-            <div v-for="product in products" :key="product.id">
-                <div class="col">
-                    <button
-                        class="card border p-2 pointer"
-                        :class="{ 'border-primary': isSelected(product.id) }"
-                        @click="toggleProductSelection(product.id)"
-                    >
-                        <img
-                            :src="product.imageUrl"
-                            :alt="'Image do produto: ' + product.name"
-                            class="m-auto"
-                            style="width: 4vw"
-                        />
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">{{ product.name }}</h5>
-                            <small class="card-text">
-                                <li class="list-group-item">
-                                    {{ product.quantity }}
-                                </li>
-                                <li class="list-group-item">
-                                    R${{ product.price.toFixed(2) }}
-                                </li>
-                            </small>
-                        </div>
-                    </button>
+    <label for="products">Selecione os produtos para a campanha</label>
+    <div class="row row-cols-1 row-cols-md-3 mt-4">
+        <div v-for="product in products" :key="product.id" class="col">
+            <button
+                class="card border p-2 pointer"
+                style="width: 9vw"
+                :class="{ 'border-primary': isSelected(product.id) }"
+                @click="toggleProductSelection(product.id)"
+            >
+                <img
+                    :src="product.imageUrl"
+                    :alt="'Image do produto: ' + product.name"
+                    class="m-auto"
+                    style="width: 4vw"
+                />
+                <div class="card-body">
+                    <p class="card-title text-truncate">
+                        {{ product.name }}
+                    </p>
+                    <small class="card-text">
+                        <li class="list-group-item">
+                            {{ product.quantity }}
+                        </li>
+                        <li class="list-group-item">
+                            R${{ product.price.toFixed(2) }}
+                        </li>
+                    </small>
                 </div>
-            </div>
+            </button>
         </div>
     </div>
 </template>
