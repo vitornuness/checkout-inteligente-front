bb99a498526bec1dcba09267349c352fda867ede
VitorNuness <vitor1nuness@gmail.com>
2024-11-22 18:06:59 -0300

    modificando classe de estilo container para container-fluid

--- src/components/home/ProductList.vue
+++ src/components/home/ProductList.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <ProductCard
                 v-for="product in products"
